7460fb53f48b1f05a0c3f8e09206b699d68e7739
Yash-Win64 <155752546+Yash-Win64@users.noreply.github.com>
2025-11-07 23:31:03 +0530

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -13,6 +13,19 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+db_url = os.getenv("DATABASE_URL")
+
+# Add SSL configuration for Aiven MySQL
+engine = create_engine(
+    db_url,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/certs/ca-certificates.crt"
+        }
+    }
+)
+
+
 # ✅ Add this function at the end
 def get_db():
     """FastAPI dependency to get a DB session."""
